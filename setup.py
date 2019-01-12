34039e60ca00aab2eac30bf91b3300f89b9726eb
Yamada <>
2019-01-12 14:25:01 +0900

    Update setup flag

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ ext_modules = cythonize(Extension("ReplayBuffer",
                                   extra_compile_args=["-std=c++17"],
                                   extra_link_args=["-std=c++17"],
                                   language="c++"),
-                        compiler_directives={'language_level':"3"})
+                        compiler_directives={'language_level':"3"},
+                        include_path=["."])
 
-setup(ext_modules = ext_modules,include_dirs=["."])
+setup(ext_modules = ext_modules,
+      include_dirs=["."],
+      cmdclass={'build_ext': build_ext})
