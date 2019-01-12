eb3100c6014f449213f917cf8e3b86da6267dcd8
Yamada <>
2019-01-13 07:29:47 +0900

    Add test for SegmentTree

---
+++ test/SegmentTree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <SegmentTree.hh>
+
+int main(){
+
+  auto st = ymd::SegmentTree<double>(16,[](auto a,auto b){ return a + b; });
+
+  for(auto i = 0ul; i < 16ul; ++i){
+    st.set(i,i*1.0);
+    std::cout << i * 1.0 << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << st.reduce(0,11) << std::endl;
+  std::cout << st.reduce(13,15) << std::endl;
+
+  return 0;
+}
