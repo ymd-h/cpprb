4867ca8df13eb9ae85cf57e2e7f2575724138b47
Yamada <>
2019-01-13 08:52:03 +0900

    Add SegmentTree into setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup,Extension
 from Cython.Build import cythonize
 
-ext_modules = cythonize(Extension("ReplayBuffer",
-                                  sources=["ReplayBuffer.pyx"],
-                                  extra_compile_args=["-std=c++17"],
-                                  extra_link_args=["-std=c++17"],
-                                  language="c++"),
-                        compiler_directives={'language_level':"3"},
-                        include_path=["."])
+ext_modules = cythonize([Extension("SegmentTree",
+                                   sources=["SegmentTree.pyx"],
+                                   extra_compile_args=["-std=c++17"],
+                                   extra_link_args=["-std=c++17"],
+                                   language="c++17"),
+                         Extension("ReplayBuffer",
+                                   sources=["ReplayBuffer.pyx"],
+                                   extra_compile_args=["-std=c++17"],
+                                   extra_link_args=["-std=c++17"],
+                                   language="c++")],
+                         compiler_directives={'language_level':"3"},
+                         include_path=["."])
 
 setup(ext_modules = ext_modules,
       include_dirs=["."],
