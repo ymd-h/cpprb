bc6cdd6a1aaaed8af220368c05415a9ffee31990
Yamada <>
2019-01-13 22:07:33 +0900

    Update: Directory Structure

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup,Extension,find_packages
 from Cython.Build import cythonize
 
-ext_modules = cythonize([Extension("SegmentTree",
-                                   sources=["SegmentTree.pyx"],
+ext_modules = cythonize([Extension("ymd_K.SegmentTree",
+                                   sources=["ymd_K/SegmentTree.pyx"],
                                    extra_compile_args=["-std=c++17"],
                                    extra_link_args=["-std=c++17"],
                                    language="c++"),
-                         Extension("ReplayBuffer",
-                                   sources=["ReplayBuffer.pyx"],
+                         Extension("ymd_K.ReplayBuffer",
+                                   sources=["ymd_K/ReplayBuffer.pyx"],
                                    extra_compile_args=["-std=c++17"],
                                    extra_link_args=["-std=c++17"],
                                    language="c++")],
                          compiler_directives={'language_level':"3"},
                          include_path=["."])
 
-setup(name = "ymd", ext_modules = ext_modules, include_dirs=["./include"],
-      packages=find_packages())
+setup(name = "ymd_K", ext_modules = ext_modules, include_dirs=["ymd_K"],
+      packages=["ymd_K"])
