51c6252bbb8cbaf03c30e4a55276bc057952d4a6
Yamada <>
2019-01-13 15:58:00 +0900

    Update: ReplayBuffer.pyx

--- ReplayBuffer.pyx
+++ ReplayBuffer.pyx
@@ -2,9 +2,11 @@
 
 from ReplayBuffer cimport ReplayBuffer as cppReplayBuffer
 
-class ReplayBuffer:
-    def __init__(self):
+cdef class ReplayBuffer:
+    def __cinit__(self,size):
         print("Hello World")
+
+        self.rb = cppReplayBuffer(size)
 
 if __name__ is "main":
     rb = ReplayBuffer()
