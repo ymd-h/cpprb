2b41d8317de44f4d9e463fc23f28324671ba65d1
Yamada <>
2019-01-14 06:19:53 +0900

    Use Internal Reserved vector

--- ymd_K/ReplayBuffer.pyx
+++ ymd_K/ReplayBuffer.pyx
@@ -4,30 +4,30 @@ from ymd_K cimport ReplayBuffer
 
 cdef class PyReplayBuffer:
     cdef ReplayBuffer[vector[double],vector[double],double,double] *thisptr
+    cdef vector[vector[double]] *obs
+    cdef vector[vector[double]] *act
+    cdef vector[double] *rew
+    cdef vector[vector[double]] *next_obs
+    cdef vector[bool] *done
     def __cinit__(self,size):
         print("Replay Buffer")
 
         self.thisptr = new ReplayBuffer[vector[double],
                                         vector[double],
                                         double,double](size)
+        self.obs = new vector[vector[double]]()
+        self.act = new vector[vector[double]]()
+        self.rew = new vector[double]()
+        self.next_obs = new vector[vector[double]]()
+        self.done = new vector[bool]()
 
     def add(self,observation,action,reward,next_observation,done):
         self.thisptr.add(observation.action,reward,next_observation,done)
 
     def sample(self,size):
-        cdef s = self.thisptr.sample(size)
-        return {'obs': get[0,tuple[vector[double],
-                                   vector[double],
-                                   double,double]](s),
-                'act': get[1,tuple[vector[double],
-                                   vector[double],
-                                   double,double]](s),
-                'rew': get[2,tuple[vector[double],
-                                   vector[double],
-                                   double,double]](s),
-                'next_obs': get[3,tuple[vector[double],
-                                        vector[double],
-                                        double,double]](s),
-                'done': get[4,tuple[vector[double],
-                                    vector[double],
-                                    double,double]](s)}
+        self.thisptr.sample(size,obs,act,rew,next_obs,done)
+        return {'obs': *obs,
+                'act': *act,
+                'rew': *rew,
+                'next_obs': *next_obs,
+                'done': *done}
