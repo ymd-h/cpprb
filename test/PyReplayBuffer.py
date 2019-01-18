c6b5fe08fbe9787377e881bd8e72cf0eb3136c49
Yamada <>
2019-01-18 11:10:40 +0900

    Add test code to ReplayBuffer.py

--- test/PyReplayBuffer.py
+++ test/PyReplayBuffer.py
@@ -1,6 +1,24 @@
 from ymd_K import ReplayBuffer
+import numpy as np
 
-obs_dim = 15
-act_dim = 10
+obs_dim = 3
+act_dim = 5
+
+N_step = 100
 
 rb = ReplayBuffer.PyReplayBuffer(15,obs_dim,act_dim)
+
+
+for i in range(N_step):
+    rb.add(np.zeros(shape=obs_dim),
+           np.ones(shape=act_dim),
+           0.5*i,
+           np.ones(shape=obs_dim)*i,
+           0 if i is not Nstep - 1 else 1)
+
+s = rb.sample(5)
+print(s['obs'])
+print(s['act'])
+print(s['rew'])
+print(s['next_obs'])
+print(s['done'])
