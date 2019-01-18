3250934da23cf9699c444043def3c49fb50c27cb
Yamada <>
2019-01-18 16:12:46 +0900

    Unit Test for PySegmentTree

--- test/PySegmentTree.py
+++ test/PySegmentTree.py
@@ -2,12 +2,22 @@ import numpy as np
 import unittest
 from ymd_K import SegmentTree
 
-sg = PySegmentTree.PySegmentTree(16,lambda a,b: a+b)
+class TestPySegmentTree(unittest.TestCase):
+    """ PySegmentTree.py """
+    N_buffer_size = 16
 
-for i in range(16):
-    sg[i] = i * 1.0
-    print("{}: {}".format(i,sg[i]))
+    @classmethod
+    def setUpClass(cls):
+        cls.sg = PySegmentTree.PySegmentTree(cls.N_buffer_size,lambda a,b: a+b)
 
-for i in range(16):
-    for j in range(i,16):
-        print("[{},{}): {}".format(i,j,sg.reduce(i,j)))
+        for i in range(cls.N_buffer_size):
+            cls.sg[i] = i * 1.0
+            print("{}: {}".format(i,cls.sg[i]))
+
+    def test_reduce(self):
+        for i in range(self.N_buffer_size):
+            for j in range(i,self.N_buffer_size):
+                self.assertAlmostEqual(0.5*(j-i)*(i*j),self.sg.reduce(i,j)))
+
+if __name__ == '__main__':
+    unittest.main()
