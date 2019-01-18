6c13e54ab47fcef78f70c4823a2e9ca577bb7582
Yamada <>
2019-01-18 16:06:30 +0900

    Fix: Constructor name

--- test/PySegmentTree.py
+++ test/PySegmentTree.py
@@ -2,7 +2,7 @@ import numpy as np
 import unittest
 from ymd_K import SegmentTree
 
-sg = PySegmentTree(16,lambda a,b: a+b)
+sg = PySegmentTree.PySegmentTree(16,lambda a,b: a+b)
 
 for i in range(16):
     sg[i] = i * 1.0
