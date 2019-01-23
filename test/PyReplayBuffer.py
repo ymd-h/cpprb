6597da4bfa843b0c8f81a301696d2d1943842f3b
Yamada <>
2019-01-24 05:51:17 +0900

    Update test code

--- test/PyReplayBuffer.py
+++ test/PyReplayBuffer.py
@@ -6,11 +6,11 @@ class TestPyReplayBuffer(unittest.TestCase):
     """=== PyReplayBuffer.py ==="""
 
     obs_dim = 3
-    act_dim = 5
+    act_dim = 1
 
     N_step = 100
-    N_buffer_size = 15
-    N_sample = 5
+    N_buffer_size = 32
+    N_sample = 16
 
     @classmethod
     def setUpClass(cls):
@@ -18,10 +18,10 @@ class TestPyReplayBuffer(unittest.TestCase):
                                              cls.obs_dim,
                                              cls.act_dim)
         for i in range(cls.N_step):
-            cls.rb.add(np.zeros(shape=(1,cls.obs_dim)),
-                       np.ones(shape=(1,cls.act_dim)),
+            cls.rb.add(np.ones(shape=(1,cls.obs_dim))*i,
+                       np.zeros(shape=(1,cls.act_dim)),
                        np.ones((1)) * 0.5*i,
-                       np.ones(shape=(1,cls.obs_dim))*i,
+                       np.ones(shape=(1,cls.obs_dim))*(i+1),
                        np.zeros((1),dtype=np.int32) if i is not cls.N_step - 1 else np.ones((1),dtype=np.int32),
                        1)
         cls.s = cls.rb.sample(cls.N_sample)
@@ -31,16 +31,19 @@ class TestPyReplayBuffer(unittest.TestCase):
         self.assertEqual(shape,array.shape)
 
     def test_obs(self):
-        self._check_ndarray(self.s['obs'],2,(self.N_sample,self.obs_dim))
+        self._check_ndarray(self.s['obs'],min(2,self.obs_dim),
+                            (self.N_sample,self.obs_dim) if self.obs_dim > 1 else (self.N_sample,))
 
     def test_act(self):
-        self._check_ndarray(self.s['act'],2,(self.N_sample,self.act_dim))
+        self._check_ndarray(self.s['act'],min(2,self.act_dim),
+                            (self.N_sample,self.act_dim) if self.act_dim > 1 else (self.N_sample,))
 
     def test_rew(self):
         self._check_ndarray(self.s['rew'],1,(self.N_sample,))
 
     def test_next_obs(self):
-        self._check_ndarray(self.s['next_obs'],2,(self.N_sample,self.obs_dim))
+        self._check_ndarray(self.s['next_obs'],min(2,self.obs_dim),
+                            (self.N_sample,self.obs_dim) if self.obs_dim > 1 else (self.obs_dim,))
 
         for i in range(self.N_sample):
             self.assertGreaterEqual(self.s['next_obs'][i,0],
@@ -58,11 +61,11 @@ class TestPyPrioritizedReplayBuffer(unittest.TestCase):
     """=== PyPrioritizedReplayBuffer.py ==="""
 
     obs_dim = 3
-    act_dim = 5
+    act_dim = 1
 
-    N_step = 1000000
-    N_buffer_size = 1000000
-    N_sample = 256
+    N_step = 100
+    N_buffer_size = 32
+    N_sample = 16
 
     alpha = 0.7
     beta = 0.5
@@ -76,10 +79,10 @@ class TestPyPrioritizedReplayBuffer(unittest.TestCase):
                                                         cls.obs_dim,
                                                         cls.act_dim)
         for i in range(cls.N_step):
-            cls.rb.add(np.zeros(shape=(1,cls.obs_dim)),
-                       np.ones(shape=(1,cls.act_dim)),
+            cls.rb.add(np.ones(shape=(1,cls.obs_dim))*i,
+                       np.zeros(shape=(1,cls.act_dim)),
                        0.5*i * np.ones((1)),
-                       np.ones(shape=(1,cls.obs_dim))*i,
+                       np.ones(shape=(1,cls.obs_dim))*(i+1),
                        np.zeros((1),dtype=np.int32) if i is not cls.N_step - 1 else np.ones((1),dtype=np.int32),
                        1)
         cls.s = cls.rb.sample(cls.N_sample,cls.beta)
@@ -96,17 +99,22 @@ class TestPyPrioritizedReplayBuffer(unittest.TestCase):
         self.assertEqual(shape,array.shape)
 
     def test_obs(self):
-        self._check_ndarray(self.s['obs'],2,(self.N_sample,self.obs_dim))
+        self._check_ndarray(self.s['obs'],min(2,self.obs_dim),
+                            (self.N_sample,self.obs_dim) if self.obs_dim > 1 else (self.N_sample,))
 
     def test_act(self):
-        self._check_ndarray(self.s['act'],2,(self.N_sample,self.act_dim))
+        self._check_ndarray(self.s['act'],min(2,self.act_dim),
+                            (self.N_sample,self.act_dim) if self.act_dim > 1 else (self.N_sample,))
 
     def test_rew(self):
         self._check_ndarray(self.s['rew'],1,(self.N_sample,))
+        print(self.s['rew'])
 
     def test_next_obs(self):
-        self._check_ndarray(self.s['next_obs'],2,(self.N_sample,self.obs_dim))
+        self._check_ndarray(self.s['next_obs'],min(2,self.obs_dim),
+                            (self.N_sample,self.obs_dim) if self.obs_dim > 1 else (self.N_sample,))
 
+        print(self.s['next_obs'])
         for i in range(self.N_sample):
             self.assertGreaterEqual(self.s['next_obs'][i,0],
                                     self.N_step - self.N_buffer_size)
