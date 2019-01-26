73a7815cfc4ad49d397cf0d5e298ba160048faa7
Yamada <>
2019-01-26 14:45:29 +0900

    Update version v2.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ ext_modules = cythonize([Extension("ymd_K.ReplayBuffer",
                          include_path=["."])
 
 setup(name = "ymd_K",
-      version="2.1.1",
+      version="2.1.2",
       ext_modules = ext_modules,
       include_dirs=["ymd_K",np.get_include()],
       packages=["ymd_K"])
