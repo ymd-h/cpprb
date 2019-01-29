0aad68791176235b4ae3b74780dbe46539a6aac4
Yamada <>
2019-01-29 18:16:41 +0900

    Update Project info from ymd_K -> cpprb

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup,Extension,find_packages
 from Cython.Build import cythonize
 import numpy as np
 
-ext_modules = cythonize([Extension("ymd_K.ReplayBuffer",
-                                   sources=["ymd_K/PyReplayBuffer.pyx"],
+ext_modules = cythonize([Extension("cpprb.ReplayBuffer",
+                                   sources=["cpprb/PyReplayBuffer.pyx"],
                                    extra_compile_args=["-std=c++17"],
                                    extra_link_args=["-std=c++17"],
                                    language="c++")],
                          compiler_directives={'language_level':"3"},
                          include_path=["."])
 
-setup(name = "ymd_K",
-      version="4.0.1",
+setup(name = "cpprb",
+      version="5.0.0",
       ext_modules = ext_modules,
-      include_dirs=["ymd_K",np.get_include()],
-      packages=["ymd_K"])
+      include_dirs=["cpprb",np.get_include()],
+      packages=["cpprb"])
