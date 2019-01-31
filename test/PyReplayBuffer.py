b4e7a1663845c95ee9aab04b8546f15e1eca7afc
Yamada <>
2019-01-31 23:14:28 +0900

    Remove duplicated code by inheritance

--- test/PyReplayBuffer.py
+++ test/PyReplayBuffer.py
@@ -93,7 +93,17 @@ class TestPyReplayBuffer(unittest.TestCase,ReplayBufferParams):
         for d in self.s['done']:
             self.assertIn(d,[0,1])
 
-class TestPyPrioritizedReplayBuffer(TestPyReplayBuffer):
+class TestPrioritizedBase:
+    def test_weights(self):
+        self._check_ndarray(self.s['weights'],1,(self.batch_size,),"weights")
+        for w in self.s['weights']:
+            self.assertAlmostEqual(w,1.0)
+
+    def test_indexes(self):
+        self._check_ndarray(self.s['indexes'],1,(self.batch_size,),"indexes")
+
+
+class TestPyPrioritizedReplayBuffer(TestPyReplayBuffer,TestPrioritizedBase):
     """=== PyPrioritizedReplayBuffer.py ==="""
     class_name = "PER"
 
@@ -113,15 +123,15 @@ class TestPyPrioritizedReplayBuffer(TestPyReplayBuffer):
         print("PER Sample {} time execution".format(cls.N_time))
         print("{} s".format(end - start))
 
-    def test_weights(self):
-        self._check_ndarray(self.s['weights'],1,(self.batch_size,),"weights")
-        for w in self.s['weights']:
-            self.assertAlmostEqual(w,1.0)
 
-    def test_indexes(self):
-        self._check_ndarray(self.s['indexes'],1,(self.batch_size,),"indexes")
+class TestNstepBase:
+    def test_discounts(self):
+        self._check_ndarray(self.s['discounts'],1,(self.batch_size,),"discounts")
+        for g,d in zip(self.s['discounts'],self.s['done']):
+            if(d > 0.0):
+                self.assertAlmostEqual(g,1.0)
 
-class TestPyNstepReplayBuffer(TestPyReplayBuffer):
+class TestPyNstepReplayBuffer(TestPyReplayBuffer,TestNstepBase):
     """=== PyNstepReplayBuffer.py ==="""
     class_name = "N-ER"
 
@@ -135,18 +145,8 @@ class TestPyNstepReplayBuffer(TestPyReplayBuffer):
         cls.fill_ReplayBuffer()
         cls.s = cls.rb.sample(cls.batch_size)
 
-    def test_done(self):
-        TestPyReplayBuffer.test_done(self)
-        for d in self.s['done']:
-            self.assertIn(d,[0,1])
-
-    def test_discounts(self):
-        self._check_ndarray(self.s['discounts'],1,(self.batch_size,),"discounts")
-        for g,d in zip(self.s['discounts'],self.s['done']):
-            if(d > 0.0):
-                self.assertAlmostEqual(g,1.0)
-
-class TestPyNstepPrioritizedReplayBuffer(TestPyReplayBuffer):
+class TestPyNstepPrioritizedReplayBuffer(TestPyReplayBuffer,
+                                         TestPrioritizedBase,TestNstepBase):
     """=== PyNstepPrioritizedReplayBuffer.py ==="""
     class_name = "N-PER"
 
@@ -156,21 +156,7 @@ class TestPyNstepPrioritizedReplayBuffer(TestPyReplayBuffer):
                                                         cls.obs_dim,
                                                         cls.act_dim,
                                                         alpha=cls.alpha)
-
-        cls.rb.add(np.ones(shape=(cls.obs_dim)),
-                   np.zeros(shape=(cls.act_dim)),
-                   0.5,
-                   np.ones(shape=(cls.obs_dim)),
-                   0)
-
-        cls.rb.clear()
-
-        for i in range(cls.N_add):
-            cls.rb.add(np.ones(shape=(cls.add_dim,cls.obs_dim))*i,
-                       np.zeros(shape=(cls.add_dim,cls.act_dim)),
-                       0.5*i * np.ones((cls.add_dim)),
-                       np.ones(shape=(cls.add_dim,cls.obs_dim))*(i+1),
-                       np.zeros((cls.add_dim)) if i is not cls.N_add - 1 else np.ones((cls.add_dim)))
+        cls.fill_ReplayBuffer()
         cls.s = cls.rb.sample(cls.batch_size,cls.beta)
 
         start = time.perf_counter()
@@ -179,52 +165,6 @@ class TestPyNstepPrioritizedReplayBuffer(TestPyReplayBuffer):
         end = time.perf_counter()
         print("N-PER Sample {} time execution".format(cls.N_time))
         print("{} s".format(end - start))
-
-    def test_obs(self):
-        self._check_ndarray(self.s['obs'],2,
-                            (self.batch_size, self.obs_dim),
-                            "obs")
-
-    def test_act(self):
-        self._check_ndarray(self.s['act'],2,
-                            (self.batch_size, self.act_dim),
-                            "act")
-
-    def test_rew(self):
-        self._check_ndarray(self.s['rew'],1,(self.batch_size,),"rew")
-
-    def test_next_obs(self):
-        self._check_ndarray(self.s['next_obs'],2,
-                            (self.batch_size, self.obs_dim),
-                            "next_obs")
-
-        for i in range(self.batch_size):
-            self.assertGreaterEqual(self.s['next_obs'][i,0],
-                                    self.N_add - self.buffer_size)
-            self.assertLess(self.s['next_obs'][i,0],self.N_add+1)
-
-            for j in range(1,self.obs_dim):
-                self.assertAlmostEqual(self.s['next_obs'][i,0],
-                                       self.s['next_obs'][i,j])
-
-    def test_done(self):
-        self._check_ndarray(self.s['done'],1,(self.batch_size,),"done")
-        for d in self.s['done']:
-            self.assertIn(d,[0,1])
-
-    def test_weights(self):
-        self._check_ndarray(self.s['weights'],1,(self.batch_size,),"weights")
-        for w in self.s['weights']:
-            self.assertAlmostEqual(w,1.0)
-
-    def test_indexes(self):
-        self._check_ndarray(self.s['indexes'],1,(self.batch_size,),"indexes")
-
-    def test_discounts(self):
-        self._check_ndarray(self.s['discounts'],1,(self.batch_size,),"discounts")
-        for g,d in zip(self.s['discounts'],self.s['done']):
-            if(d > 0.0):
-                self.assertAlmostEqual(g,1.0)
 
 if __name__ == '__main__':
     unittest.main()
