2c328f324a8ae1234409bd1c7ba621b462ca1f6c
Yamada <>
2019-02-02 20:49:42 +0900

    Revert "Rename in setup.py"
    This reverts commit 012e97094f289f371d2a86fd9032bc9677ff9b8d.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import numpy as np
 
 ext_modules = cythonize([Extension("cpprb.ReplayBuffer",
-                                   sources=["cpprb/ReplayBuffer.pyx"],
+                                   sources=["cpprb/PyReplayBuffer.pyx"],
                                    extra_compile_args=["-std=c++17"],
                                    extra_link_args=["-std=c++17"],
                                    language="c++")],
