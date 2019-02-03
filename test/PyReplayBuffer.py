59e02ae4f3d18db8f3f7c3ec3e921a214c9894dc
Yamada <>
2019-02-03 15:30:57 +0900

    Import all at test code

--- test/PyReplayBuffer.py
+++ test/PyReplayBuffer.py
@@ -1,7 +1,6 @@
 import numpy as np
 import unittest, time
-from cpprb import (ReplayBuffer,PrioritizedReplayBuffer,
-                   NstepReplayBuffer,NstepPrioritizedReplayBuffer)
+from cpprb import *
 
 class TestReplayBuffer(unittest.TestCase):
     """=== ReplayBuffer.py ==="""
