50235f38394642c01f2f9f8ed303d4d9d1e74f63
Yamada <>
2019-02-05 22:26:07 +0900

    Version 6.1.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ ext_modules = cythonize([Extension("cpprb.ReplayBuffer",
                          include_path=["."])
 
 setup(name = "cpprb",
-      version="6.1.3",
+      version="6.1.4",
       install_requires=["cython>=0.29"],
       ext_modules = ext_modules,
       include_dirs=["cpprb",np.get_include()],
