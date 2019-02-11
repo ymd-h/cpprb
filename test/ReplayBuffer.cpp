b4d2721dec04078f64d29d9fe125412d4c78f5fb
Yamada <>
2019-02-11 20:55:49 +0900

    Set timer to template function from generic lambda

--- test/ReplayBuffer.cpp
+++ test/ReplayBuffer.cpp
@@ -18,24 +18,25 @@ using Reward = double;
 using Done = double;
 using Priority = double;
 
-auto timer = [](auto&& f,std::size_t N){
-	       auto start = std::chrono::high_resolution_clock::now();
+template<typename F>
+inline auto timer(F&& f,std::size_t N){
+  auto start = std::chrono::high_resolution_clock::now();
 
-	       for(std::size_t i = 0ul; i < N; ++i){ f(); }
+  for(std::size_t i = 0ul; i < N; ++i){ f(); }
 
-	       auto end = std::chrono::high_resolution_clock::now();
-	       auto elapsed = end - start;
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = end - start;
 
-	       auto s = std::chrono::duration_cast<std::chrono::seconds>(elapsed);
-	       auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
-	       auto us = std::chrono::duration_cast<std::chrono::microseconds>(elapsed);
-	       auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed);
-	       std::cout << s.count() << "s "
-			 << ms.count() - s.count() * 1000 << "ms "
-			 << us.count() - ms.count() * 1000 << "us "
-			 << ns.count() - us.count() * 1000 << "ns"
-			 << std::endl;
-	     };
+  auto s = std::chrono::duration_cast<std::chrono::seconds>(elapsed);
+  auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
+  auto us = std::chrono::duration_cast<std::chrono::microseconds>(elapsed);
+  auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed);
+  std::cout << s.count() << "s "
+	    << ms.count() - s.count() * 1000 << "ms "
+	    << us.count() - ms.count() * 1000 << "us "
+	    << ns.count() - us.count() * 1000 << "ns"
+	    << std::endl;
+}
 
 template<typename T>
 void show_vector(T v,std::string name){
