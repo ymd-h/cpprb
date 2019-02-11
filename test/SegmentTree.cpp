adf5ac6db4432293c52160eb45cb0c0df7a3ae0e
Yamada <>
2019-02-11 14:00:22 +0900

    Update SegmentTree test

--- test/SegmentTree.cpp
+++ test/SegmentTree.cpp
@@ -27,8 +27,9 @@ auto AlmostEqual(T1&& v,T2&& expected, std::common_type_t<T1,T2>&& eps = 1e-5){
 }
 
 int main(){
+  constexpr auto buffer_size = 16;
 
-  auto st = ymd::SegmentTree<double>(16,[](auto a,auto b){ return a + b; });
+  auto st = ymd::SegmentTree<double>(buffer_size,[](auto a,auto b){ return a + b; });
 
   for(auto i = 0ul; i < 16ul; ++i){
     st.set(i,i*1.0);
@@ -44,17 +45,25 @@ int main(){
 	    << std::endl;
 
   std::cout << std::endl;
-  st.set(12,5,10);
+
+  constexpr auto set_index = 12;
+  constexpr auto set_value = 5;
+  constexpr auto set_size = 10;
+
+  st.set(set_index,set_value,set_size);
   for(auto i = 0ul; i < 16ul; ++i){
-    std::cout << st.get(i) << " ";
+    std::cout << AlmostEqual(st.get(i),(i < set_size - (buffer_size - set_index) ||
+					set_index <= i) ? set_value : i)
+	      << " ";
   }
   std::cout << std::endl;
 
-  std::cout << "[0,11): " << st.reduce(0,11) << std::endl;
-  std::cout << "[13,15): " << st.reduce(13,15) << std::endl;
+  std::cout << "[0,11): "
+	    << AlmostEqual(st.reduce(0,11),70) << std::endl;
+  std::cout << "[13,15): " << AlmostEqual(st.reduce(13,15),10) << std::endl;
 
   std::cout << "[0,x) <= 7: x = "
-	    << st.largest_region_index([](auto v){ return v <=7; })
+	    << Equal(st.largest_region_index([](auto v){ return v <=7; }),1)
 	    << std::endl;
 
   return 0;
