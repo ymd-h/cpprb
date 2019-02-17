6f447543b4558ac3a64a70a27f4b4f09c0c6eafc
Yamada <>
2019-02-17 13:40:19 +0900

    Update: Test for multiprocess

--- test/PyReplayBuffer.py
+++ test/PyReplayBuffer.py
@@ -304,49 +304,52 @@ class TestMultiProcessReplayBuffer(TestReplayBuffer):
     def setUpClass(cls):
         cls.rb = ThreadSafeReplayBuffer(cls.buffer_size, cls.obs_dim, cls.act_dim)
 
-        def f(end):
+        def f(rb,end):
             obs = np.ones(shape=(cls.obs_dim))
             act = np.zeros(shape=(cls.act_dim))
             next_obs = np.ones(shape=(cls.obs_dim))
             for i in range(0,end):
-                cls.rb.add(obs,act,1.0,next_obs, 0)
-            cls.rb.clear()
+                rb.add(obs,act,1.0,next_obs, 0)
 
-        def g(end):
+        def g(rb,end):
             obs = np.ones(shape=(cls.add_dim,cls.obs_dim))
             act = np.zeros(shape=(cls.add_dim,cls.act_dim))
             rew = np.ones((cls.add_dim))
             next_obs = np.ones(shape=(cls.add_dim,cls.obs_dim))
             done = np.zeros((cls.add_dim))
             for i in range(0,end,cls.add_dim):
-                cls.rb.add(obs,act,rew,next_obs,done)
-            cls.rb.clear()
+                rb.add(obs,act,rew,next_obs,done)
 
         def Multi_(_f):
-            def func(end):
-                q = [Process(target=_f,args=(end //8,)) for _ in range(8)]
+            def func(rb,end):
+                q = [Process(target=_f,
+                             args=(rb.init_worker(),end //8,)) for _ in range(8)]
                 for qe in q:
                     qe.start()
                 for qe in q:
                     qe.join()
             return func
 
-        def Multi_f(end):
-            Multi_(f)(end)
+        def Multi_f(rb,end):
+            Multi_(f)(rb,end)
 
-        def Multi_g(end):
-            Multi_(g)(end)
+        def Multi_g(rb,end):
+            Multi_(g)(rb,end)
 
-        timer(lambda: f(cls.N_add),cls.N_time,
+        print("Test for single thread with 1 point")
+        timer(lambda: f(cls.rb,cls.N_add),cls.N_time,
               "Single thread adding 1 time-point")
 
-        timer(lambda: g(cls.N_add),cls.N_time,
+        print("Test for single thread with 100 point")
+        timer(lambda: g(cls.rb,cls.N_add),cls.N_time,
               "Single thread adding {} time-point".format(cls.add_dim))
 
-        timer(lambda: Multi_(f)(cls.N_add),cls.N_time,
+        print("Test for multi thread with 1 point")
+        timer(lambda: Multi_(f)(cls.rb,cls.N_add),cls.N_time,
               "Multi thread adding 1 time-point")
 
-        timer(lambda: Multi_(g)(cls.N_add),cls.N_time,
+        print("Test for multi thread with 100 point")
+        timer(lambda: Multi_(g)(cls.rb,cls.N_add),cls.N_time,
               "Multi thread adding {} time-point".format(cls.add_dim))
 
         cls.fill_ReplayBuffer()
