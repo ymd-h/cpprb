153cc09c3439c1f6dd9fd7e629612602d14c9b8b
Yamada <>
2019-04-07 14:17:39 +0900

    Add file exsits switch for sdist compilation

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
+import os
 from setuptools import setup,Extension,find_packages
 from Cython.Build import cythonize
 import numpy as np
 
-ext_modules = cythonize([Extension("cpprb.ReplayBuffer",
-                                   sources=["cpprb/PyReplayBuffer.pyx"],
-                                   extra_compile_args=["-std=c++17",
-                                                       "-march=native"],
-                                   extra_link_args=["-std=c++17","-pthread"],
-                                   language="c++"),
-                         Extension("cpprb.VectorWrapper",
-                                   sources=["cpprb/VectorWrapper.pyx"],
-                                   extra_compile_args=["-std=c++17",
-                                                       "-march=native"],
-                                   extra_link_args=["-std=c++17","-pthread"],
-                                   language="c++")],
-                         compiler_directives={'language_level':"3"},
-                         include_path=["."])
+if os.path.exists("cpprb/PyReplayBuffer.pyx"):
+    ext_modules = cythonize([Extension("cpprb.ReplayBuffer",
+                                       sources=["cpprb/PyReplayBuffer.pyx"],
+                                       extra_compile_args=["-std=c++17",
+                                                           "-march=native"],
+                                       extra_link_args=["-std=c++17","-pthread"],
+                                       language="c++"),
+                             Extension("cpprb.VectorWrapper",
+                                       sources=["cpprb/VectorWrapper.pyx"],
+                                       extra_compile_args=["-std=c++17",
+                                                           "-march=native"],
+                                       extra_link_args=["-std=c++17","-pthread"],
+                                       language="c++")],
+                            compiler_directives={'language_level':"3"},
+                            include_path=["."])
+else:
+    ext_modules = [Extension("cpprb.ReplayBuffer",
+                             sources=["cpprb/PyReplayBuffer.cpp"],
+                             extra_compile_args=["-std=c++17",
+                                                 "-march=native"],
+                             extra_link_args=["-std=c++17","-pthread"],
+                             language="c++"),
+                   Extension("cpprb.VectorWrapper",
+                             sources=["cpprb/VectorWrapper.cpp"],
+                             extra_compile_args=["-std=c++17",
+                                                 "-march=native"],
+                             extra_link_args=["-std=c++17","-pthread"],
+                             language="c++")]
 
 setup(name = "cpprb",
       author="Yamada Hiroyuki",
