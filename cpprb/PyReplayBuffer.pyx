fd0e70eec7b0b58fcc640c87ec0573bf5022831e
Yamada <>
2019-04-19 05:55:39 +0900

    Remove cython.int from Fused types

--- cpprb/PyReplayBuffer.pyx
+++ cpprb/PyReplayBuffer.pyx
@@ -14,10 +14,10 @@ from .VectorWrapper import (VectorWrapper,VectorInt,VectorSize_t,VectorDouble,Po
 Obs      = cython.fused_type(cython.float, cython.double)
 Act      = cython.fused_type(cython.float, cython.double)
 Rew      = cython.fused_type(cython.float, cython.double,
-                             cython.short, cython.int, cython.long)
+                             cython.short, cython.long)
 Next_Obs = cython.fused_type(cython.float, cython.double)
 Done     = cython.fused_type(cython.float, cython.double,
-                             cython.short, cython.int, cython.long)
+                             cython.short, cython.long)
 Prio     = cython.fused_type(cython.float, cython.double)
 
 cdef class Environment:
