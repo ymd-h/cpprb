748a024d3e7fe779b5fa3e1963d55c9f796390ff
Yamada <>
2019-04-20 11:48:44 +0900

    Use unittest for issue40

--- test/issue40.py
+++ test/issue40.py
@@ -1,21 +1,27 @@
 import numpy as np
+import unittest
 
 from cpprb import ReplayBuffer
 
-buffer_size = 256
-obs_dim = 3
-act_dim = 1
-rb = ReplayBuffer(buffer_size,obs_dim,act_dim)
+class TestIssue40(unittest.TestCase):
+    def test(self):
+        buffer_size = 256
+        obs_dim = 3
+        act_dim = 1
+        rb = ReplayBuffer(buffer_size,obs_dim,act_dim)
 
-obs = np.ones(shape=(obs_dim))
-act = np.ones(shape=(act_dim))
-rew = 0
-next_obs = np.ones(shape=(obs_dim))
-done = 0
+        obs = np.ones(shape=(obs_dim))
+        act = np.ones(shape=(act_dim))
+        rew = 0
+        next_obs = np.ones(shape=(obs_dim))
+        done = 0
 
-for i in range(500):
-    rb.add(obs,act,rew,next_obs,done)
+        for i in range(500):
+            rb.add(obs,act,rew,next_obs,done)
 
 
-batch_size = 32
-sample = rb.sample(batch_size)
+        batch_size = 32
+        sample = rb.sample(batch_size)
+
+if __name__ == '__main__':
+    unittest.main()
