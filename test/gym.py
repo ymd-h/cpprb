3787a5efecbf705711ec852b96b94bb0e50835c5
Yamada <>
2019-04-21 17:22:07 +0900

    Add gym test

---
+++ test/gym.py
@@ -0,0 +1,17 @@
+import numpy as np
+import unittest
+
+from cpprb import gym
+
+
+class TestGym(unittest.TestCase):
+
+    def test_NoteBookAnimation(self):
+        a = gym.NotebookAnimation()
+
+    def test_Animation(self):
+        with self.assertRaises(TypeError):
+            gym.Animation()
+
+if __name__ == '__main__':
+    unittest.main()
