afcc434048e352ad80b3a1f4c772db9949295f8e
Yamada <>
2019-05-03 17:40:39 +0900

    EpsilonGreedy Class

--- cpprb/rl/algorithm.py
+++ cpprb/rl/algorithm.py
@@ -15,3 +15,15 @@ class GreedyPolicy:
 
     def __call__(self,obs,*args,**kwargs):
         return np.argmax(self.model.predict(obs))
+
+class EpsilonGreedyPolicy(RandomPolicy,GreedyPolicy):
+    def __init__(self,env,model,eps = 0.1,*args,**kwargs):
+        RandomPolicy.__init__(self,env,*args,**kwargs)
+        GreedyPolicy.__init__(self,model,*args,**kwargs)
+        self.eps = eps
+
+    def __call__(self,obs,*args,**kwargs):
+        if np.random.rand() < eps:
+            RandomPolicy.__call__(self,obs,*args,**kwargs)
+        else:
+            GreedyPolicy.__call__(self,obs,*args,**kwargs)
