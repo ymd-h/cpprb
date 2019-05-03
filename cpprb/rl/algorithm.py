8c928896f08ae43a91c474b1ce4d4c342f68c6a2
Yamada <>
2019-05-03 17:35:07 +0900

    Add: GreedyPolicy and fit __init__ with RandomPolicy

--- cpprb/rl/algorithm.py
+++ cpprb/rl/algorithm.py
@@ -3,8 +3,15 @@ import numpy as np
 from cpprb import explore
 
 class RandomPolicy:
-    def __init__(self,env):
+    def __init__(self,env,*args,**kwargs):
         self.act_dim = env.action_space.n
 
     def __call__(self,*args,**kwargs):
         return np.random.choices(self.act_dim)
+
+class GreedyPolicy:
+    def __init__(self,model,*args,**kwargs):
+        self.model = model
+
+    def __call__(self,obs,*args,**kwargs):
+        return np.argmax(self.model.predict(obs))
