40fdc772b71bdb4a33e7156fa7badf3da3984c68
Yamada <>
2019-05-06 10:25:42 +0900

    Accept flexible obs_shape

--- cpprb/PyReplayBuffer.pyx
+++ cpprb/PyReplayBuffer.pyx
@@ -1315,13 +1315,13 @@ def explore(buffer,policy,env,n_iteration,*,
     cdef size_t LOCAL = local_buffer
     cdef size_t LONGEST = longest_step
 
-    obs = np.zeros((LOCAL,buffer.get_obs_dim()),dtype=np.double)
+    obs = np.zeros((LOCAL,* buffer.get_obs_shape()),dtype=np.double)
     next_obs = np.zeros_like(obs)
     act = np.zeros((LOCAL,buffer.get_act_dim()),dtype=np.double)
     rew = np.zeros((LOCAL,buffer.get_rew_dim()),dtype=np.double)
     done = np.zeros((LOCAL),dtype=np.double)
 
-    cdef double [:,:] o = obs
+    cdef double [:,:] o = obs.reshape(LOCAL,-1)
     cdef double [:,:] a = act
     cdef double [:,:] r = rew
     cdef double [:,:] no= next_obs
