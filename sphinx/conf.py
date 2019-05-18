f326029ef19f3a47acbcfb2b496e36188377bd51
Yamada <>
2019-05-18 21:20:49 +0900

    Use admonition for examples, notes, references

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -2,3 +2,6 @@ extensions = ['sphinx.ext.napoleon']
 html_theme = "nature"
 
 napoleon_include_init_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
