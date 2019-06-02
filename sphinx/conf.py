45e45726225c4c46cde2556c84014c7ea01ba4f8
Yamada <>
2019-06-02 17:10:38 +0900

    Add logo and favicon into sphinx

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -5,6 +5,9 @@ copyright = "2018, Hiroyuki Yamada"
 extensions = ['sphinx.ext.napoleon']
 html_theme = "sphinx_rtd_theme"
 
+html_logo = "../site/static/images/logo.png"
+html_favicon = "../site/static/images/favicon.png"
+
 napoleon_include_init_with_doc = True
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
