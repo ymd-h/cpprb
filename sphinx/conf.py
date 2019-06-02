9883e0cb4387102b4955ee723d1a4c197af50077
Yamada <>
2019-06-02 19:01:31 +0900

    Update: autodoc_default_flags
    Show __init__, __cinit__

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -12,3 +12,12 @@ napoleon_include_init_with_doc = True
 napoleon_use_admonition_for_examples = True
 napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
+
+autodoc_default_flags = {
+    'members': None,
+    'undoc-members': None,
+    'special-members': '__init__,__cinit__',
+    'show-inheritance': None,
+    'member-order': 'bysource',
+    'exclude-members': '__dict__,__weakref__,__module__'
+}
