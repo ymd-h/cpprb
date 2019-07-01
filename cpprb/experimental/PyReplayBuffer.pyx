2c38256fc4345b8f05f54dee2a11d30705437eff
Yamada <>
2019-07-01 18:51:35 +0900

    Store tmporal value stored_b
    Reduce dictionary access to self.buffer[name]

--- cpprb/experimental/PyReplayBuffer.pyx
+++ cpprb/experimental/PyReplayBuffer.pyx
@@ -225,11 +225,12 @@ cdef class NstepBuffer:
         gamma *= self.gamma
         if self.Nstep_rew is not None:
             for name in self.Nstep_rew:
+                stored_b = self.buffer[name]
                 ext_b = self._extract(kwargs,name)
 
                 copy_ext = ext_b.copy()
                 if diff_N:
-                    self.buffer[name][self.stored_size:] = ext_b[:diff_N]
+                    stored_b[self.stored_size:] = ext_b[:diff_N]
                     ext_b = ext_b[diff_N:]
 
                 for i in range(self.stored_size-1,
@@ -239,7 +240,7 @@ cdef class NstepBuffer:
                     copy_ext[ext_begin:] *= gamma[stored_begin:i+N]
                     ext_b[stored_begin:i+N] += copy_ext[ext_begin:]
 
-                self._roll(self.buffer[name],b,end,NisBigger,kwargs,name,add_N)
+                self._roll(stored_b,b,end,NisBigger,kwargs,name,add_N)
 
         for name, stored_b in self.buffer.items():
             if self.Nstep_rew is not None and np.isin(name,self.Nstep_rew).any():
