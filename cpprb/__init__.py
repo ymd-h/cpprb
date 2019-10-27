6f8752986cf8a1de73da19ed18d6d4104c248195
Yamada <>
2019-10-27 16:07:50 +0900

    Fix: Delete non-existing class

--- cpprb/__init__.py
+++ cpprb/__init__.py
@@ -1,6 +1,3 @@
 from .PyReplayBuffer import (ReplayBuffer,PrioritizedReplayBuffer,
-                             NstepReplayBuffer,NstepPrioritizedReplayBuffer,
-                             SelectiveReplayBuffer,
-                             ProcessSharedReplayBuffer,
-                             ProcessSharedPrioritizedReplayBuffer)
+                             SelectiveReplayBuffer)
 from .PyReplayBuffer import create_buffer, explore
