33afafa7027efdb138d3186789b66adcad830b60
Yamada <>
2020-02-09 21:05:57 +0900

    Fix: Stop base class test

--- test/util.py
+++ test/util.py
@@ -5,9 +5,7 @@ import gym
 from cpprb import ReplayBuffer
 from cpprb.util import create_env_dict
 
-class TestAlgorithms(unittest.TestCase):
-    @unittest.skipIf(type(self) == TestAlgorithms,
-                     "TestAlgorithms is abstract class")
+class TestAlgorithms:
     def test_create_dict(self):
         env_dict = create_env_dict(self.env)
 
@@ -16,8 +14,6 @@ class TestAlgorithms(unittest.TestCase):
         self.assertIn("act1",env_dict)
         self.assertIn("act2",env_dict)
 
-    @unittest.skipIf(type(self) == TestAlgorithms,
-                     "TestAlgorithms is abstract class")
     def test_add(self):
         env_dict = create_env_dict(self.env)
         rb = ReplayBuffer(256,env_dict)
@@ -42,27 +38,27 @@ class TestAlgorithms(unittest.TestCase):
                 obs = next_obs
 
 
-class TestCopy(TestAlgorithms):
+class TestCopy(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("Copy-v0")
 
-class TestDuplicatedInput(TestAlgorithms):
+class TestDuplicatedInput(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("DuplicatedInput-v0")
 
-class TestRepeatCopy(TestAlgorithms):
+class TestRepeatCopy(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("RepeatCopy-v0")
 
-class TestReverse(TestAlgorithms):
+class TestReverse(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("Reverse-v0")
 
-class TestReversedAddition(TestAlgorithms):
+class TestReversedAddition(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("ReversedAddition-v0")
 
-class TestReversedAddition3(TestAlgorithms):
+class TestReversedAddition3(TestAlgorithms,unittest.TestCase):
     def setUp(self):
         self.env = gym.make("ReversedAddition3-v0")
 
