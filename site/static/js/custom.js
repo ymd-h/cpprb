e244c62db7d2f336ee0e1bbf6ddc5f6d619e5a26
Yamada <>
2020-02-12 05:54:47 +0900

    Add custom.js for star count

---
+++ site/static/js/custom.js
@@ -0,0 +1,18 @@
+(function(){
+    const path = location.pathname
+
+    var gl = $.get("https://gitlab.com/api/v4/projects/ymd_h%2Fcpprb");
+
+    $(document).ready(function(){
+	if(gl){
+	    gl.then((data)=>{
+		$("section#shorcuts a.padding > i.fa-gitlab").parent()
+		    .append("(<i class='fas fa-star fa-fw'></i>"
+			    + data["star_count"]
+			    + "<i class='fas fa-code-branch fa-fw'></i>"
+			    + data["forks_count"]
+			    + ")");
+	    });
+	}
+    });
+})();
