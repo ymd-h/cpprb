afbdd016265bc9430285cef409f751c39062b2bb
Yamada <>
2020-04-22 07:41:38 +0900

    UnitTest for ReplayBuffer multiple step add without done

--- test/issue.py
+++ test/issue.py
@@ -214,6 +214,24 @@ class TestIssue61(unittest.TestCase):
 
         rb.sample(batch_size)
 
+    def test_ReplayBuffer_with_multiple_steps(self):
+        buffer_size = 256
+        obs_shape = (3,4)
+        step_size = 32
+        batch_size = 10
+
+        rb = ReplayBuffer(buffer_size,{"obs": {"shape": obs_shape}})
+
+        v = {"obs": np.ones(shape=(step_size,*obs_shape))}
+
+        rb.add(**v)
+
+        rb.sample(batch_size)
+
+        for _ in range(100):
+            rb.add(**v)
+
+        rb.sample(batch_size)
 
 if __name__ == '__main__':
     unittest.main()
