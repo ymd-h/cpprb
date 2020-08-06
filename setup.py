d19a9b611ae92df0103df14e6f06fc0ce4a5d7ae
Yamada <ymd_h@outlook.jp>
2020-08-07 06:57:49 +0900

    Version 9.2.1

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ else:
 setup(name="cpprb",
       author="Yamada Hiroyuki",
       description=description,
-      version="9.2.0",
+      version="9.2.1",
       install_requires=requires,
       setup_requires=setup_requires,
       extras_require=extras,
