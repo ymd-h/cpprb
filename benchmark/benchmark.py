fb0e71e9533e661a8259f9f3d1559047de1e1882
Yamada <ymd_h@outlook.jp>
2020-08-19 06:58:40 +0900

    Fix: Use [] for scaler value

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -84,9 +84,9 @@ def add_r(_rb):
         for i in range(e["obs"].shape[0]):
             _rb.add(SampleBatch(obs_t=list(e["obs"][i]),
                                 action=list(e["act"][i]),
-                                reward=list(e["rew"][i]),
+                                reward=[e["rew"][i]],
                                 obs_tp1=list(e["next_obs"][i]),
-                                done=list(e["done"][i])),
+                                done=[e["done"][i]]),
                     weight=0.5)
     return add
 
