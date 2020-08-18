890ec094b059649ba62a1e773cb1e60b92540b10
Yamada <ymd_h@outlook.jp>
2020-08-19 07:08:52 +0900

    Use [] for numpy too, to match the size of list.

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -82,10 +82,10 @@ def add_r(_rb):
     """
     def add(e):
         for i in range(e["obs"].shape[0]):
-            _rb.add(SampleBatch(obs_t=list(e["obs"][i]),
-                                action=list(e["act"][i]),
+            _rb.add(SampleBatch(obs_t=[e["obs"][i]]),
+                                action=[e["act"][i]],
                                 reward=[e["rew"][i]],
-                                obs_tp1=list(e["next_obs"][i]),
+                                obs_tp1=[e["next_obs"][i]],
                                 done=[e["done"][i]]),
                     weight=0.5)
     return add
