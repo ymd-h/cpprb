42bad4f9a0ab9643feb9c1a85c8ed473a8d89176
Yamada <ymd_h@outlook.jp>
2020-08-19 07:32:13 +0900

    Fix: parenthis

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -82,11 +82,11 @@ def add_r(_rb):
     """
     def add(e):
         for i in range(e["obs"].shape[0]):
-            _rb.add(SampleBatch(obs_t=[e["obs"][i]]),
+            _rb.add(SampleBatch(obs_t=[e["obs"][i]],
                                 action=[e["act"][i]],
                                 reward=[e["rew"][i]],
                                 obs_tp1=[e["next_obs"][i]],
-                                done=[e["done"][i]],
+                                done=[e["done"][i]]),
                     weight=0.5)
     return add
 
