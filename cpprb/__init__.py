4bf93fb0468b170eeab130bbf3f6511f8a6d5d9f
Yamada <ymd_h@outlook.jp>
2020-10-03 19:17:33 +0900

    Add comment for no gym case

--- cpprb/__init__.py
+++ cpprb/__init__.py
@@ -5,5 +5,6 @@ from .PyReplayBuffer import create_buffer, train
 try:
     from .util import create_env_dict, create_before_add_func
 except ImportError:
+    # If gym is not installed, util functions are not defined.
     pass
 
