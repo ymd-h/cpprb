087f1dd250eb2147f90f4ce1b7895ecd0738acfa
Yamada <ymd_h@outlook.jp>
2021-03-19 08:29:49 +0900

    Add smaller test

--- test/save_load.py
+++ test/save_load.py
@@ -27,10 +27,34 @@ class TestReplayBuffer(unittest.TestCase):
         rb1.save_transitions(fname)
         rb2.load_transitions(fname)
 
-        t1 = rb.get_all_transitions()
-        t2 = rb.get_all_transitions()
+        t1 = rb1.get_all_transitions()
+        t2 = rb2.get_all_transitions()
 
         np.testing.assert_allclose(t1["a"], t2["a"])
+
+    def test_smaller_buffer(self):
+        """
+        Load to smaller buffer
+
+        Loaded buffer only stored last buffer_size transitions
+        """
+        buffer_size1 = 10
+        buffer_size2 = 4
+        env_dict = {"a": {}}
+
+        rb1 = ReplayBuffer(buffer_size1, env_dict)
+        rb2 = ReplayBuffer(buffer_size2, env_dict)
+
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        fname = "smaller.gz"
+        rb1.save_transitions(fname)
+        rb2.load_transitions(fname)
+
+        t1 = rb1.get_all_transitions()
+        t2 = rb2.get_all_transitions()
+
+        np.testing.assert_allclose(t1["a"][-buffer_size2:],t2["a"])
 
 if __name__ == "__main__":
     unittest.main()
