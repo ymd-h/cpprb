45574599da7bdcd77722631a54593d033bf4ee59
Yamada <ymd_h@outlook.jp>
2021-03-20 11:58:03 +0900

    Test: incompatible load_transitions

--- test/save_load.py
+++ test/save_load.py
@@ -224,6 +224,32 @@ class TestReplayBuffer(unittest.TestCase):
 
         np.testing.assert_allclose(t1["a"], t2["a"])
 
+    def test_incompatible_stack_compress(self):
+        """
+        Load incompatible stack_compress transitions with safe mode
+        """
+        buffer_size = 10
+        env_dict = {"a": {"shape": 3}}
+
+        rb1 = ReplayBuffer(buffer_size, env_dict, stack_compress="a")
+        rb2 = ReplayBuffer(buffer_size, env_dict)
+
+        a = [[1, 2, 3],
+             [2, 3, 4],
+             [3, 4, 5],
+             [4, 5, 6]]
+
+        rb1.add(a=a)
+
+        fname="incompatible_stack_compress.npz"
+        rb1.save_transitions(fname)
+        rb2.load_transitions(fname)
+
+        t1 = rb1.get_all_transitions()
+        t2 = rb2.get_all_transitions()
+
+        np.testing.assert_allclose(t1["a"], t2["a"])
+
 
 if __name__ == "__main__":
     unittest.main()
