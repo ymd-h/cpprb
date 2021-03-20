2d4f8159647d578e5d7b5f400d50afe8f0f8a5ff
Yamada <ymd_h@outlook.jp>
2021-03-20 17:12:00 +0900

    Check index size

--- cpprb/PyReplayBuffer.pyx
+++ cpprb/PyReplayBuffer.pyx
@@ -1182,13 +1182,15 @@ cdef class ReplayBuffer:
 
         idx = 0
         for i in cache_idx:
-            self.add(**{k: v[idx:i] for k,v in d.items()},
-                     **{f"next_{k}": d[k][idx+1:i+1] for k in n})
+            if idx < i:
+                self.add(**{k: v[idx:i] for k,v in d.items()},
+                         **{f"next_{k}": d[k][idx+1:i+1] for k in n})
             self.add(**{**{k: v[i] for k,v in d.items()}, **c[i]})
             idx = i+1
 
-        self.add(**{k: v[idx:_size] for k,v in d.items()},
-                 **{f"next_{k}": d[k][idx+1:_size+1] for k in n})
+        if idx < _size:
+            self.add(**{k: v[idx:_size] for k,v in d.items()},
+                     **{f"next_{k}": d[k][idx+1:_size+1] for k in n})
 
         if N:
             self.use_nstep = True
