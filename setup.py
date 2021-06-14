daaa11c09535bf3c9fd78525426bb50398e4f0b8
Yamada <ymd_h@outlook.jp>
2021-06-14 21:37:37 +0900

    Version 10.1.1

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ else:
 setup(name="cpprb",
       author="Yamada Hiroyuki",
       description=description,
-      version="10.1.0",
+      version="10.1.1",
       install_requires=requires,
       setup_requires=setup_requires,
       extras_require=extras,
