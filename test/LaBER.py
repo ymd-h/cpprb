13b4a66f46307e08494630f4122208570d655668
Yamada <ymd_h@outlook.jp>
2021-10-16 10:59:21 +0900

    Add LaBER test script

---
+++ test/LaBER.py
@@ -0,0 +1,34 @@
+import unittest
+
+from cpprb import LaBERmean, LaBERlazy, LaBERmax
+
+class TestLaBER:
+    def test_negative(self):
+        with unittest.assertRaises(ValueError):
+            self.cls(-12)
+
+        with unittest.assertRaises(ValueError):
+            self.cls(12, -4)
+
+        with unittest.assertRaises(ValueError):
+            self.cls(12, 4, -2)
+
+class TestLaBERmean(TestLaBER, unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cls = LaBERmean
+
+class TestLaBERlazy(TestLaBER, unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cls = LaBERlazy
+
+
+class TestLaBERmax(TestLaBER, unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cls = LaBERmax
+
+
+if __name__ == "__main__":
+    unittest.main()
