023c9a8e4c91e44eaa8ff62da7c94007869a3210
Yamada <ymd_h@outlook.jp>
2021-10-16 13:11:15 +0900

    Add normal test

--- test/LaBER.py
+++ test/LaBER.py
@@ -4,11 +4,27 @@ from cpprb import LaBERmean, LaBERlazy, LaBERmax
 
 class TestLaBER:
     def test_init(self):
+
+        laber = self.cls(12)
+        self.assertEqual(laber.batch_size, 12)
+        self.assertEqual(laber.idx, [i for i in range(12*4)])
+        self.assertEqual(laber.eps, 1e-6)
+
         with self.assertRaises(ValueError):
             self.cls(-12)
 
+        laber = self.cls(12, 5)
+        self.assertEqual(laber.batch_size, 12)
+        self.assertEqual(laber.idx, [i for i in range(12*5)])
+        self.assertEqual(laber.eps, 1e-6)
+
         with self.assertRaises(ValueError):
             self.cls(12, -4)
+
+        laber = self.cls(12, 5, eps=1e-4)
+        self.assertEqual(laber.batch_size, 12)
+        self.assertEqual(laber.idx, [i for i in range(12*5)])
+        self.assertEqual(laber.eps, 1e-4)
 
         with self.assertRaises(ValueError):
             self.cls(12, 4, eps=-2)
