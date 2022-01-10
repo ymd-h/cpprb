c8e15f85b900708244657b406aad2c759a4a82fb
Yamada <ymd_h@outlook.jp>
2022-01-10 13:49:21 +0900

    Add multistep test

--- test/jax_test.py
+++ test/jax_test.py
@@ -15,6 +15,17 @@ class TestJAX(unittest.TestCase):
             with self.subTest(i=i):
                 rb.add(done=done)
 
+    def test_multistep_add(self):
+        import jax.numpy as jnp
+        rb = ReplayBuffer(4, {"done": {}})
+
+        done = jnp.asarray([1,1,1])
+
+        for i in range(2):
+            with self.subTest(i=i):
+                rb.add(done=done)
+
+
     def test_nstep(self):
         import jax.numpy as jnp
         rb = ReplayBuffer(6, {"obs": {}, "rew": {}, "done": {}, "next_obs":{}},
@@ -28,8 +39,19 @@ class TestJAX(unittest.TestCase):
         for i in range(7):
             with self.subTest(i=i):
                 rb.add(obs=obs, rew=rew, done=done, next_obs=next_obs)
-                rb.add(obs=obs, rew=rew, done=done, next_obs=next_obs)
-                rb.add(obs=obs, rew=rew, done=done, next_obs=next_obs)
+
+    def tet_nstep_multistep_add(self):
+        import jax.numpy as jnp
+        rb = ReplayBuffer(6, {"obs": {}, "rew": {}, "done": {}, "next_obs":{}},
+                          Nstep={"size": 4, "rew": "rew", "next": "next_obs"})
+
+        obs = jnp.asarray([1,1,1,1])
+        rew = jnp.asarray([1,1,1,1])
+        done = jnp.asarray([1,1,1,1])
+        next_obs = jnp.asarray([1,1,1,1])
+
+        for i in range(7):
+            with self.subTest(i=i):
                 rb.add(obs=obs, rew=rew, done=done, next_obs=next_obs)
 
 
