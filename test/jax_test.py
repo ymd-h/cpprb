b201eaef4d376a61e35146eebd720821b857ba5e
Yamada <ymd_h@outlook.jp>
2022-01-10 15:08:44 +0900

    Take out import statement

--- test/jax_test.py
+++ test/jax_test.py
@@ -1,12 +1,16 @@
 import sys
 import unittest
 
+try:
+    import jax.numpy as jnp
+except ImportError:
+    pass
+
 from cpprb import ReplayBuffer
 
 @unittest.skipIf(sys.platform.startswith("win"), "JAX doesn't support Windows")
 class TestJAX(unittest.TestCase):
     def test_add(self):
-        import jax.numpy as jnp
         rb = ReplayBuffer(4, {"done": {}})
 
         done = jnp.asarray(1)
@@ -16,7 +20,6 @@ class TestJAX(unittest.TestCase):
                 rb.add(done=done)
 
     def test_multistep_add(self):
-        import jax.numpy as jnp
         rb = ReplayBuffer(4, {"done": {}})
 
         done = jnp.asarray([1,1,1])
@@ -27,7 +30,6 @@ class TestJAX(unittest.TestCase):
 
 
     def test_nstep(self):
-        import jax.numpy as jnp
         rb = ReplayBuffer(6, {"obs": {}, "rew": {}, "done": {}, "next_obs":{}},
                           Nstep={"size": 4, "rew": "rew", "next": "next_obs"})
 
@@ -41,7 +43,6 @@ class TestJAX(unittest.TestCase):
                 rb.add(obs=obs, rew=rew, done=done, next_obs=next_obs)
 
     def tet_nstep_multistep_add(self):
-        import jax.numpy as jnp
         rb = ReplayBuffer(6, {"obs": {}, "rew": {}, "done": {}, "next_obs":{}},
                           Nstep={"size": 4, "rew": "rew", "next": "next_obs"})
 
