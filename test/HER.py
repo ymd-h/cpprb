c2f541b229d3434e343bce3c11a5612cfb9c4f28
Yamada <ymd_h@outlook.jp>
2022-02-03 18:50:55 +0900

    Update unit test

--- test/HER.py
+++ test/HER.py
@@ -44,6 +44,148 @@ class TestHER(unittest.TestCase):
         hrb.clear()
         self.assertEqual(hrb.get_stored_size(), 0)
 
+    def test_future(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "future",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+        hrb.on_episode_end(3)
+        self.assertEqual(hrb.get_stored_size(), 6)
+
+        sample = hrb.get_all_transitions()
+        self.assertIsIn("rew", sample)
+        self.assertIsIn("goal", sample)
+        self.assertEqual(sample["obs"].shape, (6,1))
+        self.assertTrue((sample["obs"] < sample["goal"]).all())
+        np.testing.assert_allclose(sample["rew"],
+                                   rew_func(sample["obs"],
+                                            sample["act"],
+                                            sample["goal"]))
+
+    def test_episode(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "episode",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+        hrb.on_episode_end(3)
+        self.assertEqual(hrb.get_stored_size(), 6)
+
+        sample = hrb.get_all_transitions()
+        self.assertIsIn("rew", sample)
+        self.assertIsIn("goal", sample)
+        self.assertEqual(sample["obs"].shape, (6,1))
+        np.testing.assert_allclose(sample["rew"],
+                                   rew_func(sample["obs"],
+                                            sample["act"],
+                                            sample["goal"]))
+
+
+    def test_random(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "random",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+        hrb.on_episode_end(3)
+        self.assertEqual(hrb.get_stored_size(), 6)
+
+        sample = hrb.get_all_transitions()
+        self.assertIsIn("rew", sample)
+        self.assertIsIn("goal", sample)
+        self.assertEqual(sample["obs"].shape, (6,1))
+        np.testing.assert_allclose(sample["rew"],
+                                   rew_func(sample["obs"],
+                                            sample["act"],
+                                            sample["goal"]))
+
+    def test_final(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "final",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+
+        # additonal_goals is ignored
+        self.assertEqual(hrb.additional_goals, 1)
+
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+        hrb.on_episode_end(3)
+        self.assertEqual(hrb.get_stored_size(), 4)
+
+        sample = hrb.get_all_transitions()
+        self.assertIsIn("rew", sample)
+        self.assertIsIn("goal", sample)
+        self.assertEqual(sample["obs"].shape, (4,1))
+        np.testing.assert_allclose(sample["goal"][2:],
+                                   np.broadcast_to(2, (2,1)))
+
+
+    def test_unknown_strategy(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        with self.assertRaises(ValueError):
+            hrb = HindsightReplayBuffer(size=10,
+                                        env_dict={"obs": {},"act": {},"next_obs": {}},
+                                        max_episode_len=2,
+                                        strategy = "__UNKNOWN_STRATEGY__",
+                                        reward_func=rew_func,
+                                        additonal_goals=2,
+                                        prioritized=False)
+
+
+    def test_assert_exceed_max_episode(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "future",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+        with self.assertRaises(ValueError):
+            hrb.add(obs=2, act=0, next_obs=3)
+
+    def test_assert_PER(self):
+        rew_func = lambda s,a,g: -1*(s==g)
+        hrb = HindsightReplayBuffer(size=10,
+                                    env_dict={"obs": {}, "act": {}, "next_obs": {}},
+                                    max_episode_len=2,
+                                    strategy = "future",
+                                    reward_func=rew_func,
+                                    additonal_goals=2,
+                                    prioritized=False)
+
+        hrb.add(obs=0, act=0, next_obs=1)
+        hrb.add(obs=1, act=0, next_obs=2)
+
+        with self.assertRaises(ValueError):
+            hrb.get_max_priority()
+
+        with self.assertRaises(ValueError):
+            hrb.update_priorities([], [])
 
 if __name__ == "__main__":
     unittest.main()
