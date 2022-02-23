6cebd65333e5d75c50ddab7ca4d17f37ab5975af
Yamada <ymd_h@outlook.jp>
2022-02-24 08:23:00 +0900

    Use SharedMemoryManager

--- cpprb/multiprocessing.py
+++ cpprb/multiprocessing.py
@@ -11,17 +11,33 @@ if _has_SharedMemory:
     # serialization/deserialization by using unique name. This
     # capability allows users to use buffers in Ray (https://ray.io/).
     from multiprocessing.shared_memory import SharedMemory
+    from multiprocessing.managers import SharedMemoryManager
+
+    _manager = None
+
+    def get_manager():
+        global _manager
+
+        if _manager is None:
+            _manager = SharedMemoryManager()
+            _manager.start()
+
+            @atexit.register
+            def shutdown(*args):
+                _manager.shutdown()
+
+        return _manager
+
+
     class _RawValue:
         def __init__(self, ctype, init=None):
             size = ctypes.sizeof(ctype)
-            self.shm = SharedMemory(create=True, size=size)
+            self.shm = get_manager().SharedMemory(size=size)
             self.dtype = np.dtype(ctype)
 
             self.ndarray = np.ndarray((1,), self.dtype, buffer=self.shm.buf)
             if init is not None:
                 self.ndarray[0] = init
-
-            self.owner = True
 
         @property
         def value(self):
@@ -37,30 +53,18 @@ if _has_SharedMemory:
         def __setstate__(self, shm_dtype):
             self.shm, self.dtype = shm_dtype
             self.ndarray = np.ndarray((1,), self.dtype, buffer=self.shm.buf)
-            self.owner = False
 
-        def __del__(self):
-            self.shm.close()
-            if self.owner:
-                self.shm.unlink()
 
     class _RawArray:
         def __init__(self, ctype, len):
             size = ctypes.sizeof(ctype) * len
-            self.shm = SharedMemory(create=True, size=size)
-            self.owner = True
+            self.shm = get_manager().SharedMemory(size=size)
 
         def __getstate__(self):
             return self.shm
 
         def __setstate__(self, shm):
             self.shm = shm
-            self.owner = False
-
-        def __del__(self):
-            self.shm.close()
-            if self.owner:
-                self.shm.unlink()
 
         def __len__(self):
             return len(self.shm)
@@ -80,6 +84,7 @@ if _has_SharedMemory:
         @property
         def buf(self):
             return self.shm.buf
+
 
 
 def RawArray(ctx, ctype, len, backend="sharedctypes"):
