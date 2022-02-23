94389982f5f89840e60e6c5656346ed99e5cb97a
Yamada <ymd_h@outlook.jp>
2022-02-23 22:25:11 +0900

    Internal _RawArray class

--- cpprb/multiprocessing.py
+++ cpprb/multiprocessing.py
@@ -12,14 +12,14 @@ if _has_SharedMemory:
     # capability allows users to use buffers in Ray (https://ray.io/).
     from multiprocessing.shared_memory import SharedMemory
     class _RawValue:
-        def __init__(self, ctx, ctype_, init_=None):
-            size = ctypes.sizeof(ctype_)
+        def __init__(self, ctype, init=None):
+            size = ctypes.sizeof(ctype)
             self.shm = SharedMemory(create=True, size=size)
-            self.dtype = np.dtype(ctype_)
+            self.dtype = np.dtype(ctype)
 
             self.ndarray = np.ndarray((1,), self.dtype, buffer=self.shm.buf)
-            if init_ is not None:
-                self.ndarray[0] = init_
+            if init is not None:
+                self.ndarray[0] = init
 
             self.owner = True
 
@@ -44,24 +44,53 @@ if _has_SharedMemory:
             if self.owner:
                 self.shm.unlink()
 
+    class _RawArray:
+        def __init__(self, ctype, len):
+            size = ctypes.sizeof(ctype) * len
+            self.shm = SharedMemory(create=True, size=size)
+            self.owner = True
+
+        def __getstate__(self):
+            return self.shm
+
+        def __setstate__(self, shm):
+            self.shm = shm
+            self.owner = False
+
+        def __del__(self):
+            self.shm.close()
+            if self.owner:
+                self.shm.unlink()
+
+        def __len__(self):
+            return len(self.shm)
+
+        def __getitem__(self, i):
+            return self.shm[i]
+
+        def __setitem__(self, i, value):
+            self.shm[i] = value
+
+        def __getslice__(self, start, stop):
+            return self.shm[start:stop]
+
+        def __setslice__(self, start, stop, values):
+            self.shm[start:stop] = values
+
+        @property
+        def buf(self):
+            return self.shm.buf
+
 
 def RawArray(ctx, ctype, len, backend="sharedctypes"):
     if _has_SharedMemory and backend == "SharedMemory":
-        size = ctypes.sizeof(ctype) * len
-        shm = SharedMemory(create=True, size=size)
-
-        @atexit.register
-        def _cleanup():
-            shm.close()
-            shm.unlink()
-
-        return shm
+        return _RawArray(ctype, len)
     else:
         return ctx.Array(ctype, len, lock=False)
 
 
 def RawValue(ctx, ctype, init, backend="sharedctypes"):
     if _has_SharedMemory and backend == "SharedMemory":
-        return _RawValue(ctx, ctype, init)
+        return _RawValue(ctype, init)
     else:
         return ctx.Value(ctype, init, lock=False)
