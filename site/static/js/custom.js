9ef75a0b6e98ce71f980e3cef84a11d7ff8400a9
Yamada <ymd_h@outlook.jp>
2022-03-05 12:34:53 +0900

    Fix: Replace unintentional tab with spaces

--- site/static/js/custom.js
+++ site/static/js/custom.js
@@ -5,26 +5,26 @@
     var gh = $.get("https://api.github.com/repos/ymd-h/cpprb");
 
     $(document).ready(function(){
-	if(gl){
-	    gl.then((data)=>{
-		$("section#shortcuts a.padding > i.fa-gitlab").parent()
-		    .append(" (<i class='fas fa-star fa-fw'></i>"
-			    + data["star_count"]
-			    + "<i class='fas fa-code-branch fa-fw'></i>"
-			    + data["forks_count"]
-			    + ")");
-	    });
-	}
+        if(gl){
+            gl.then((data)=>{
+                $("section#shortcuts a.padding > i.fa-gitlab").parent()
+                    .append(" (<i class='fas fa-star fa-fw'></i>"
+                            + data["star_count"]
+                            + "<i class='fas fa-code-branch fa-fw'></i>"
+                            + data["forks_count"]
+                            + ")");
+            });
+        }
 
-	if(gh){
-	    gh.then((data)=>{
-		$("section#shortcuts a.padding > i.fa-github").parent()
-		    .append(" (<i class='fas fa-star fa-fw'></i>"
-			    + data["stargazers_count"]
-			    + "<i class='fas fa-code-branch fa-fw'></i>"
-			    + data["forks_count"]
-			    + ")");
-	    });
-	}
+        if(gh){
+            gh.then((data)=>{
+                $("section#shortcuts a.padding > i.fa-github").parent()
+                    .append(" (<i class='fas fa-star fa-fw'></i>"
+                            + data["stargazers_count"]
+                            + "<i class='fas fa-code-branch fa-fw'></i>"
+                            + data["forks_count"]
+                            + ")");
+            });
+        }
     });
 })();
