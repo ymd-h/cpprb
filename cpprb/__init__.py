1c7df62c4ea27c96abcd388c459e087908823e64
Yamada <ymd_h@outlook.jp>
2022-03-10 23:29:05 +0900

    Add module docstring

--- cpprb/__init__.py
+++ cpprb/__init__.py
@@ -1,3 +1,37 @@
+"""
+cpprb: Fast Flexible Replay Buffer Library
+
+cpprb provides replay buffer classes for reinforcement learning.
+Details are described at `Home Page <https://ymd_h.gitlab.io/cpprb/>`_.
+
+Examples
+--------
+Replay Buffer classes can be imported from ``cpprb`` package.
+
+>>> from cpprb import ReplayBuffer
+
+These buffer classes can be created by specifying ``env_dict``.
+
+>>> env_dict = {"obs": {}, "act": {}, "rew": {}, "next_obs": {}, "done": {}}
+>>> rb = ReplayBuffer(1e+6, env_dict)
+
+When adding transitions, all values must be passed as keyword arguments.
+
+>>> rb.add(obs=1, act=1, rew=0.5, next_obs=2, done=0)
+
+You can also add multiple transitions simultaneously.
+
+>>> rb.add(obs=[1,2], act=[1,2], rew=[0.5,0.3], next_obs=[2,3], done=[0,1])
+
+At the episode end, users must call ``on_episode_end()`` method.
+
+>>> rb.on_episode_end()
+
+Transitions can be sampled according to these buffer's algorithms (e.g. random).
+
+>>> sample = rb.sample(32)
+"""
+
 from .PyReplayBuffer import (ReplayBuffer,PrioritizedReplayBuffer,
                              MPReplayBuffer, MPPrioritizedReplayBuffer,
                              SelectiveReplayBuffer, ReverseReplayBuffer)
