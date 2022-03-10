9fa572599223b35c65db586e83116815b5aaa090
Yamada <ymd_h@outlook.jp>
2022-03-10 12:14:40 +0900

    Remove colon

--- cpprb/PyReplayBuffer.pyx
+++ cpprb/PyReplayBuffer.pyx
@@ -1059,7 +1059,7 @@ cdef class ReplayBuffer:
 
         Returns
         -------
-        : int or None
+        int or None
             The first index of stored position. If all transitions are stored
             into ``NstepBuffer`` and no transtions are stored into the main buffer,
             ``None`` is returned.
