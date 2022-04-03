f611a58092a9ce20498b6f6babab1b45985190bd
Yamada <ymd_h@outlook.jp>
2022-04-03 15:06:52 +0900

    Fix expected shape

--- test/issue.py
+++ test/issue.py
@@ -1021,7 +1021,7 @@ class TestIssue143(unittest.TestCase):
                              {"a": {"shape": (3,3)},
                               "b": {"shape": (3,3)}},
                              stack_compress=["a", "b"])
-        self.assertEqual(buffer["a"].shape, (10 +3 -1, 3))
+        self.assertEqual(buffer["a"].shape, (10, 3, 3))
 
 if __name__ == '__main__':
     unittest.main()
