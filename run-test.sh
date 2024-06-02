e0205181f634ba6196bf2056062d0281f9713fc6
H.Yamada <ymd_h@outlook.jp>
2024-06-02 19:04:25 +0900

    Fix: Ensure Installation on test

--- run-test.sh
+++ run-test.sh
@@ -3,8 +3,13 @@
 set -eux
 
 hatch run cov:erase
+
+hatch run test:install
+hatch run test-38:install
+
 hatch run test:run-cov
 hatch run test-38:run-cov
+
 hatch run cov:combine
 hatch run cov:report
 
